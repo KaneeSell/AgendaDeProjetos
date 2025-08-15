ec1cbfd1995d08688a9f040c782616906cd160f2
Daniel Henrique Sell <kanee.sell@gmail.com>
2025-08-15 10:04:36 -0300

    style: loading size

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 export default function Loading(){
     return(
         <div className="z-20 w-full h-screen fixed flex items-center justify-center bg-black/40 light:text-white">
-            <AiOutlineLoading3Quarters size={'20px'} className="animate-spin"/>
+            <AiOutlineLoading3Quarters size={'40px'} className="animate-spin"/>
         </div>
     )
 }
